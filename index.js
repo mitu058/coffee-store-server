d9a35d0020b611441d5c82b7113655ada2107e9a
mitu058 <mitum9796@gmail.com>
2024-11-30 20:28:07 +0600

    add user in database and delete user from database

--- index.js
+++ index.js
@@ -35,6 +35,8 @@ async function run() {
     await client.connect();
     const coffeeCollection = client.db("coffeeDB").collection("coffee")
 
+    const userCollection = client.db("coffeeDB").collection("users")
+
 // read/get data to localhost
 app.get('/coffee',async(req,res)=>{
   const cursor = coffeeCollection.find()
@@ -85,6 +87,40 @@ app.delete('/coffee/:id', async(req,res)=>{
   const id = req.params.id
   const query = {_id : new ObjectId(id)}
   const result = await coffeeCollection.deleteOne(query)
+  res.send(result)
+})
+
+// users related apis
+// show users in localhost
+app.get('/users',async(req,res)=>{
+  const cursor = userCollection.find()
+  const users = await cursor.toArray()
+  res.send(users)
+})
+
+app.post('/users', async(req,res)=>{
+  const newUser = req.body
+  console.log(newUser);
+  const result = await userCollection.insertOne(newUser)
+  res.send(result)
+})
+
+app.patch('/users',async(req,res)=>{
+  const email = req.body.email
+  const filter = {email}
+  const updateUser = {
+    $set: {
+      lastSignInTime: req.body?.lastSignInTime
+    }
+  }
+  const result = await userCollection.updateOne(filter,updateUser)
+  res.send(result)
+})
+
+app.delete('/users/:id', async(req,res)=>{
+  const id = req.params.id
+  const query = {_id : new ObjectId(id)}
+  const result = await userCollection.deleteOne(query)
   res.send(result)
 })
 
